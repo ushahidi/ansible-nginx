c5b48fa02d450c1aecc1d6db39743abe9be50962
David Losada <david@tuxpiper.com>
2017-11-24 12:21:34 +0100

    update compliance to Ansible 2.4 . update tests

--- server/molecule/default/tests/test_default.py
+++ server/molecule/default/tests/test_default.py
@@ -1,7 +1,9 @@
 import testinfra.utils.ansible_runner
+import os
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
 
 def test_nginx_running(Process, Socket):
     p = Process.get(user="root", comm="nginx")
